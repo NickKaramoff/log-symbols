786f37bed33ecd81f353b504e453a69a13e02626
Nikita Karamov <me@kytta.dev>
2021-12-07 15:38:54 +0100

    Remove testing .d.ts
    The `check-dts` command takes a long time to execute and requires
    TypeScript to also be installed. Since the symbols are all strings, it
    doesn't make much sense to load such an amount of dev dependencies.

--- test/index.errors.ts
+++
@@ -1,7 +0,0 @@
-import lib from "..";
-
-// THROWS The right-hand side of an arithmetic operation must be of type 'any', 'number', 'bigint' or an enum type.
-console.log(10 * lib.info);
-
-// THROWS Property 'sort' does not exist on type 'string'.
-console.log(lib.info.sort());
--- test/index.types.ts
+++
@@ -1,6 +0,0 @@
-import lib from "..";
-
-console.log(lib.error);
-console.log(lib.info);
-console.log(lib.success);
-console.log(lib.warning);
