1b1e08de88cd190eb017e2d2bfeecfdca70554d2
Nikita Karamov <me@kytta.dev>
2021-12-07 15:36:07 +0100

    Simplify unit tests
     - remove dependency on `strip-ansi` — instead check for substring in
       the output
    
     - remove unused TERM environment variable setting
    
     - launch tests using `uvu` command

--- index.test.js
+++ index.test.js
@@ -1,19 +1,16 @@
 import { test } from "uvu";
 import * as assert from "uvu/assert";
-import stripAnsi from "strip-ansi";
-import nanoSymbols from "../index.js";
+import nanoSymbols from "./index.js";
 
 for (const [key, value] of Object.entries(nanoSymbols)) {
   console.log(value, key);
 }
 
-console.log("");
+console.log();
 
 test("returns log symbols", () => {
-  process.env.TERM = "xterm-256color";
   assert.ok(
-    stripAnsi(nanoSymbols.success) === "✔" ||
-      stripAnsi(nanoSymbols.success) === "√"
+    nanoSymbols.success.includes("✔") || nanoSymbols.success.includes("√")
   );
 });
 
